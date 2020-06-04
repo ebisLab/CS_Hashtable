080661b58a4d0512fe406e72a1de3750bc4d3591
ebisLab <ebaiden@techie.com>
2020-06-04 07:30:42 -0400

    expensive sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,24 @@
 # Your code here
+# ~~~~~revisit
+
+# similar to the fibbonaci
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
+    key = (x, y, z)  # touple
+    if key in cache:
+        cache[key]
+    if x <= 0:  # or y <= 0 or z <= 0
+        return y+z
 
+    # x,y,z greater than or equal to 0
+
+    if x >= 0 or y >= 0 or z >= 0:
+        cache[key] = expensive_seq(
+            x-1, y+1, z)+expensive_seq(x-2, y+2, z*2)+expensive_seq(x-3, y+3, z*3)
+    return cache[key]
 
 
 if __name__ == "__main__":
